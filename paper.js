a4a7989f07ec489c05fd645da896e44d010edd83
vedansh06 <67477921+vedansh06@users.noreply.github.com>
2020-09-27 17:35:35 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,29 @@
+class Paper {
+ 
+    constructor(x,y,diameter){
+      var options={
+          isStatic: false,
+          restitution: 0.3,
+          friction: 0.5,
+          density: 1.2
+      }
+      this.body = Bodies.circle(x, y, diameter, options);
+      this.diameter = diameter;
+      
+      World.add(world, this.body);
+    }
+
+    display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    ellipseMode(BOTTOM);
+    fill("purple");
+    ellipse(0, 0, this.diameter);
+    pop(); 
+
+    }
+
+}
